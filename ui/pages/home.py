ad53ec0641ffe591f6742677e66947e195d794d1
Ed Balogh <ed.balogh@gmail.com>
2024-07-23 19:32:58 -0400

    Update home.py

--- ui/pages/home.py
+++ ui/pages/home.py
@@ -1,12 +1,12 @@
 import os
 from nicegui import APIRouter, ui
 from langserve import RemoteRunnable
-
 from templates.page_layout import page_layout
 from templates.chatbot.chat import ChatBot
 
 router = APIRouter()
 API_URL = os.environ['API_URL']
+
 
 def parse_response_fn(bot_message):
     bot_data_str = ""
@@ -14,29 +14,9 @@ def parse_response_fn(bot_message):
     print(f"bot_message: {bot_message}, type={type(bot_message)}", flush=True)
     if type(bot_message) != tuple:
         if type(bot_message) == dict:
-            if bot_message.get('content') != "":
-                bot_data_str += bot_message.get('content') + "\n"
-            if bot_message.get('raw'):
-                bot_message_content = bot_message.get('raw')
-
-                if type(bot_message_content) == dict:
-                    print(bot_message_content)
-
-        if type(bot_message).__name__ == 'AIMessage':
-            bot_message_content = bot_message.content
-            if type(bot_message_content) == str:
-                bot_data_str += bot_message_content + "\n"
-            elif type(bot_message_content) == list:
-                for content_item in bot_message_content:
-                    if content_item.get('type'):
-                        content_item_type = content_item.get('type')
-                        if content_item_type == 'text':
-                            bot_data_str += content_item.get('text') + "\n"
-                        elif content_item_type == 'tool_use':
-                            if content_item.get('name') == 'create_df_from_cypher':
-                                bot_data_str += "```cypher\n" + content_item.get('input').get('cypher_query') + "\n```\n"
-                            elif content_item.get('name') == 'code_interpreter':
-                                bot_data_str += "```python\n" + content_item.get('input').get('code') + "\n```\n"
+            if bot_message.get('messages', '') != '':
+                for m in bot_message.get('messages'):
+                    bot_data_str += m.get('content', '') + "\n"
 
         bot_data_str += "\n\n"
 
